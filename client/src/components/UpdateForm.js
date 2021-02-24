bb6a167524a4d7e129485be949e0b2b750a35cdf
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-24 13:03:03 -0500

    lecture work-in-progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -13,7 +13,9 @@ const initialItem = {
 const UpdateForm = props => {
   const [item, setItem] = useState(initialItem);
   const id = 1;
-  
+
+  console.log(props);
+
   useEffect(()=>{
     axios.get(`http://localhost:3333/items/${id}`)
     .then((res)=>{
